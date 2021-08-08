fa51be5a5f29c00a20fb7e0f1d165b4c20dc12bb
dPacc <themdk01@gmail.com>
2021-08-09 00:34:29 +0400

    create: post and get posts

---
+++ posts-microservice/posts/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const { randomBytes } = require("crypto");
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+
+// Store posts in memory instead of a database
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.json(posts);
+});
+
+app.post("/posts", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  // Save the title of post to the posts object
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  res.json(posts[id]);
+});
+
+app.listen(4000, () => {
+  console.log("Listening on 4000");
+});
