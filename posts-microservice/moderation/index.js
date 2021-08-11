718cc8943a6dde6affe8a00260b6adf36b213a86
dPacc <themdk01@gmail.com>
2021-08-11 18:55:04 +0400

    add: comment moderation route and send event

--- posts-microservice/moderation/index.js
+++ posts-microservice/moderation/index.js
@@ -3,10 +3,31 @@ const axios = require("axios");
 
 const app = express();
 
-// Middlewares
+// Middleware
 app.use(express.json());
 
-// Run the server
+// Routes
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "CommentCreated") {
+    const status = data.content.includes("orange") ? "rejected" : "approved";
+
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentModerated",
+      data: {
+        id: data.id,
+        content: data.content,
+        postId: data.postId,
+        status,
+      },
+    });
+  }
+
+  res.send({});
+});
+
+// Start server
 app.listen(4003, () => {
-  console.log("Server running on port 4003");
+  console.log("Listening on port 4003");
 });
